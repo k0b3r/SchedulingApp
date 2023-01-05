7b3bee39f186febd513de5f19b43250ec2ae78b3
Karoline Oberhansley <Koberhansley@yahoo.com>
2023-01-04 20:29:47 -0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module schrader.schedulingapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens schrader.schedulingapp to javafx.fxml;
+    exports schrader.schedulingapp;
+}
